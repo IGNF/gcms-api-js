d137eed4a72c9d864911a5274a973977e9f06ea2
msotgia <madeline.sotgia@ign.fr>
2014-06-05 14:45:12 +0200

    intégration keepChanges strategie

--- src/GCMS/Layer/VectorLayer.js
+++ src/GCMS/Layer/VectorLayer.js
@@ -55,7 +55,8 @@ GCMS.Layer.VectorLayer = OpenLayers.Class(
 				}
 				else{
 					layerOptions.strategies = [ 
-					     new OpenLayers.Strategy.BBOX({ratio: 1.2,resFactor: 2})
+					     new OpenLayers.Strategy.BBOX({ratio: 1.2,resFactor: 2}),
+					     new GCMS.Strategy.KeepChanges()
                     ];
 				}
 				
